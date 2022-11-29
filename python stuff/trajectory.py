5cb36157c12929ef75747a642f88f95707f68735
Haricharan1212 <haricharan12122003@gmail.com>
2022-11-29 21:40:49 +0530

    29th Nov

--- python stuff/trajectory.py
+++ python stuff/trajectory.py
@@ -41,6 +41,8 @@ def omega_5(w, t):
     - 4 * a4*np.sin(4*t*w) + 4 * b4*np.cos(4*t*w) - 5 *  a5*np.sin(5*t*w) + 5 * b5 * np.cos(5*t*w) - 
     6 * a6*np.sin(6*t*w) + 6 * b6 * np.cos(6*t*w))
 
+
+
 def theta_6(w, t): 
     
     a0 =       -0.27
@@ -91,3 +93,10 @@ def omega_6(w, t):
     - 4 * a4*np.sin(4*t*w) + 4 * b4*np.cos(4*t*w) - 5 *  a5*np.sin(5*t*w) + 5 * b5 * np.cos(5*t*w) - 
     6 * a6*np.sin(6*t*w) + 6 * b6 * np.cos(6*t*w) - 7 * a7*np.sin(7*w*t) + 7 * b7 * np.cos(7*w*t) 
     - 8 * a8 * np.sin(8*w*t) + 8 * b8 * np.cos(8 * w * t))
+
+# from matplotlib import pyplot as plt
+# import numpy as np
+
+# t = np.array([i/100 for i in range(500)])
+# plt.plot(t, theta_5(1.7, t))
+# plt.show()
